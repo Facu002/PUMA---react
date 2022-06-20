f4b3afd00f09a0ee36edb83d5a2fea5d976753e5
Facu002 <achafacundoagustin@gmail.com>
2022-06-20 15:40:51 -0300

    new-design-v0.3

---
+++ src/components/Banner.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import img1 from '../img/slider_1.jpg'
+
+function Banner(props) {
+  //let colRef = query(collection(db, "BannerImages"));
+  
+  // useEffect(()=>{
+  //   // get collection data
+    
+  //     getDocs(colRef) 
+  //       .then(snapshot => {
+  //           // console.log(snapshot.docs)
+  //           snapshot.docs.forEach(doc => {
+  //           list.push({ ...doc.data()})
+  //           })
+  //           setImages(list)
+  //           //console.log(list);
+  //           //console.log('1')
+  //           //console.log('2')
+  //           //caca()
+  //       })
+  //       .catch(err => {
+  //           console.log(err.message)
+  //       })
+  // },[])
+
+      return (
+        <div className='Banner'>
+            <div className="Banner-img__container">
+              <img id="counter" className='Banner-img' src={img1} alt="caca"  />
+          </div>
+        </div>
+      )
+}
+
+export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import MainDisplay from '../components/MainDisplay'
+import Banner from '../components/Banner'
 function Home() {
     return (
-        <>
+        <>  
+            <Banner/>
             <MainDisplay/>
         </>
     )
