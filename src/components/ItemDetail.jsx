0966ffdcf52a3abad2a7dbe86bd6b2a5addefff9
Facu002 <achafacundoagustin@gmail.com>
2022-06-25 16:23:10 -0300

    new-design-v0.5 (finally product section)

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,43 +1,46 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection, query, where } from "firebase/firestore";
 import { db } from './firebaseConfig';
 import ItemInfo from './ItemInfo';
 function ItemDetail() {
     let {id} = useParams()
     const [products, setProducts] = useState([])
+
+    
     useEffect(()=>{
-        let fetchOne = async()=>{
-            const docRef = doc(db, "products", `${id}`);
+        async function fetchOne(){
+            const docRef = doc(db, "products", `${id}`)
+
             const docSnap = await getDoc(docRef);
+                
             if (docSnap.exists()) {
-                return {
-                    id: id,
-                    ...docSnap.data()
-                }
+                console.log("Document data:", docSnap.data());
+                setProducts(docSnap.data())
+
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
             }
         }
         fetchOne()
-        .then(result=>setProducts(result))
-        .catch(err=>console.log(err))
-        
-    },[])
+    },[id])
+
     return (
-        <>
-        {
+        <div>
+        { products !== [] ? 
                 <ItemInfo
-                //key={products.id}
+                key={products.id}
                 description={products.description}
                 name={products.name}
-                img={products.images}
-                
+                imgs={products.images}
                 stock={products.stock}
                 price={products.price}
                 />
-                
+            : null
         }
-        </>
+        </div>
     )
 }
 
--- src/components/ItemInfo.jsx
+++ src/components/ItemInfo.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 
 function ItemInfo(props) {
-
+  if (props !== undefined)
   return (
-    <div className='itemDetail'>
-            <h1>{props.name}</h1>
-            <img className='imageShow' ></img>
-            <div className="imageCollection">
-                
-            </div>
-    </div> 
-  )
+      <div className='itemDetail'>
+        {
+        props.imgs  ?
+                    
+          <>
+          <h1>{props.name}</h1>
+          <img className='imageShow' src={props.imgs[0]} alt='ok'></img>
+          <div className="imageCollection">
+            <img className='imageShow' src={props.imgs[1]} alt="caca" />
+            <img className='imageShow' src={props.imgs[2]} alt="caca" />
+            <img className='imageShow' src={props.imgs[3]} alt="caca" />
+          </div>
+          <span>{props.description}</span>
+        </>
+        : console.log('nada')
+        }   
+      </div> 
+    )
+  else
+    return (
+        {props}
+      )
+  // 
 }
 
 export default ItemInfo
