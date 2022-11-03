fcec81d8a739c745317cc26ae679be700c9803e6
Facu002 <achafacundoagustin@gmail.com>
2022-11-03 15:24:18 -0300

    new-design-v14 (added banner slide)

---
+++ src/components/Banner.js
@@ -0,0 +1,86 @@
+import {React, useEffect} from 'react'
+import img0 from '../img/slider_0.jpg'
+import img1 from '../img/slider_1.jpg'
+import img2 from '../img/slider_2.jpg'
+function Banner(props) {
+  //let colRef = query(collection(db, "BannerImages"));
+  
+  // useEffect(()=>{
+  //   // get collection data
+    
+  //     getDocs(colRef) 
+  //       .then(snapshot => {
+  //           // console.log(snapshot.docs)
+  //           snapshot.docs.forEach(doc => {
+  //           list.push({ ...doc.data()})
+  //           })
+  //           setImages(list)
+  //           //console.log(list);
+  //           //console.log('1')
+  //           //console.log('2')
+  //           //caca()
+  //       })
+  //       .catch(err => {
+  //           console.log(err.message)
+  //       })
+  // },[])
+  
+  useEffect(() => {
+    let slider = document.querySelector('.slider-banner-container');
+    let sliderIndividual = document.querySelectorAll('.banner-img__container');
+    console.log(sliderIndividual);
+    let index = 1;
+    let screenWidth = sliderIndividual[0].clientWidth;
+    let interval = 5000;
+
+    window.addEventListener('resize', function(params) {
+        screenWidth = sliderIndividual[0].clientWidth;
+    })
+
+    
+
+    function slidesAnimation() {
+        slider.style.transform = 'translateX(-'+(screenWidth*index)+'px)'
+        slider.style.transition = 'all 0.8s'
+        index++
+
+        if (index == sliderIndividual.length) {
+          setTimeout(() => {
+            slider.style.transform = 'translateX(0px)'
+            slider.style.transition = 'none'
+            index=1
+          }, 1500);
+        }
+    }
+
+    return () => {
+      setInterval(() => {
+        slidesAnimation()
+    }, interval);
+    }
+  }, [])
+  
+
+      return (
+        <div className='banner'>
+          <div className="slider-banner-container">
+            <div className="banner-img__container">
+              <img id="counter" className='banner-img' src={img0} alt="banner"  />
+            </div>
+            <div className="banner-img__container">
+              <img id="counter" className='banner-img' src={img1} alt="banner"  />
+            </div>
+            <div className="banner-img__container">
+              <img id="counter" className='banner-img' src={img2} alt="banner"  />
+            </div>
+            <div className="banner-img__container">
+              <img id="counter" className='banner-img' src={img0} alt="banner"  />
+            </div>
+
+          </div>
+
+        </div>
+      )
+}
+
+export default Banner
